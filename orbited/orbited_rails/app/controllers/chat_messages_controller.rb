aef09e06dcb489fa243f1d32170befe58c1e6352
Alberto Perdomo <alberto.perdomo@aentos.es>
2010-03-05 18:14:38 +0000

    Changed sent message to be added to the messages div with the word received.

--- orbited/orbited_rails/app/controllers/chat_messages_controller.rb
+++ orbited/orbited_rails/app/controllers/chat_messages_controller.rb
@@ -3,7 +3,7 @@ class ChatMessagesController < ApplicationController
   def create
     @message = ChatMessage.create(params[:chat_message])
     data = render_to_string :update do |page|
-      page.alert @message.content
+      page['messages'].insert :bottom => "<div class='message'>#{Time.now.strftime("%I:%M%p")} - Received: #{@message.content}</div>"
     end
     Orbited.send_data('hello', data)
   end
