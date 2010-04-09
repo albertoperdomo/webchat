eb39ccc10300332f3567e1f17a6524526b3e525a
Ancor Cruz <contactar@ancorcruz.com>
2010-04-09 19:27:10 +0800

    chat messages and user list interfaces are improved

--- orbited/rails_xmpp/public/javascripts/chat.js
+++ orbited/rails_xmpp/public/javascripts/chat.js
@@ -5,10 +5,13 @@
 xmpp = new XMPPClient();
 xmpp.onPresence = function(ntype, from, to) {
     if (! ntype) {
-      alert("User " + from + " available");
+      
+      //alert("User " + from + " available");
+      $('#user_list').append("<li class='contact' id='budy_" + from.split('/')[0] + "'>" + from.split('/')[0] + "</li>");
     }
     else if (ntype == "unavailable") {
-      alert("User " + from + " unavailable");
+      //alert("User " + from + " unavailable");
+      $('#budy_' + $.escape(from.split('/')[0])).remove();
     }
     else if (ntype == "subscribe") {
       xmpp.sendSubscribed(from, to);
@@ -73,14 +76,20 @@ function connectFailure() {
 function onMessage(jid, username, text) {
   // update the UI to reflect the message received
   // alert("Received message from " + username + ":\n" + text);
-  $('#' + $.escape(jid.split('/')[0])).append("<p><span class='user_sender'>" + username.split('/')[0] + ":</span> " + text);
+  if ($('#chat_' + $.escape(jid.split('/')[0])).length == 0) {
+    $('#chat_box').append('<div id="chat_' + jid.split('/')[0] + '" style="height:100px;border:1px solid black;overflow:scroll;"></div>');
+  }
+  $('#chat_' + $.escape(jid.split('/')[0])).append("<p><span class='user_sender'>" + username.split('/')[0] + ":</span> " + text);
 }
 
 /** Called when the user types a message to be sent */
 function onSendMessage(toJid, toUsername, text) {
   // update the UI to reflect the message the user just sent
   // alert("Sending a message to " + toJid + ":\n" + text);
-  $('#' + $.escape(toJid)).append("<p><span class='self'>me:</span> " + text);
+  if ($('#chat_' + $.escape(toJid)).length == 0) {
+    $('#chat_box').append('<div id="chat_' + toJid + '" style="height:100px;border:1px solid black;overflow:scroll;"></div>');
+  }
+  $('#chat_' + $.escape(toJid)).append("<p><span class='self'>me:</span> " + text);
   xmpp.msg(toJid, text);
 }
 
