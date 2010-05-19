c312c4b617c6d99d0e16dd8902c0972f95d28701
Ancor Cruz <contactar@ancorcruz.com>
2010-05-20 02:48:34 +0800

    Add to roster conctacts (senders) with unreaded messages when chat opens

--- orbited/rails_xmpp/app/controllers/chat_controller.rb
+++ orbited/rails_xmpp/app/controllers/chat_controller.rb
@@ -4,6 +4,8 @@ class ChatController < ApplicationController
     current_user.reset_single_access_token!
     @contact = User.find(params[:contact_id])
 
+    @contacts_to_rosterize = current_user.received_messages.unreaded.collect {|msg| msg.sender }.uniq
+
     respond_to do |format|
       format.js
     end
